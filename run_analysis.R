9c326b9f7c7646bd82f74ec935558da3e3e4feb4
John Skinner <jno.skinner@gmail.com>
2015-08-23 17:57:55 -0500

    Remove unneeded head() print command

--- run_analysis.R
+++ run_analysis.R
@@ -2,8 +2,8 @@ library(reshape2) # Install with command `install.packages('reshape2')` if missi
 
 # Get names for the features
 feature_names <- read.table("UCI HAR Dataset/features.txt", header = FALSE, col.names = c("id", "name"))
-head(feature_names)
 
+# These are the indexes at which mean and standard deviation data lives
 data_indexes <- c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,227:228,240:241,253:254,266:271,345:350,424:429,503:504,516:517,529:530,542:543)
 
 # Get test data and compose into a dataframe
